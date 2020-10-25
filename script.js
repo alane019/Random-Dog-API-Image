b5ee4ae24abed0d078131ae9ad862802571c0575
Adam <alane019@gmail.com>
2020-10-24 23:22:35 -0400

    updated image api load function and button

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 
-           // add dog image
+           // adds dog image to img element 
+         let nextDog = function() {
             dogApiImg = fetch('https://dog.ceo/api/breeds/image/random')
             .then(response => response.json())
-            .then(data =>  { document.querySelector("#imgWrap").innerHTML = `<img src="${data.message}" alt="doggo">`});
+            .then(data =>  { document.querySelector("#targetImg").src = data.message});
+           }
+      window.onload = function() {
+         document.getElementById("dogButton").addEventListener("click", nextDog);
+         nextDog();
+      }
